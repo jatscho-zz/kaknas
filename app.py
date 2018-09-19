d6f07b4fdea54b57b5938b92bdfd070cdb5eaf9b
Zach Yam <zach.yam@cognite.com>
2018-09-19 14:20:37 +0200

    First push

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    # r = requests.get(
-    #     "https://api.github.com/users/zachyam/repos",
-    #     headers={"Authorization": "access_token 11b1b74c676365dfb6fedc03322839d77bb335a2"},
-    # )
     return "Hello"
 
 
