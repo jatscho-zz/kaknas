79cbab437970e624f7354e449348e116ea1ce700
Zach Yam <zach.yam@cognite.com>
2018-09-20 17:22:39 +0200

    Add health check (#2)
    * Add health check
    
    * Add gitignore
    
    * Remove secrets

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def hello_world():
     return "Hello"
 
+@app.route("/health")
+def health_check():
+    return "200 OK"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
